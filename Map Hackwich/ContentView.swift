b9cca8b7a1d9083ed536902901965f96374a48b6
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2024-02-23 09:44:33 -0600

    "Added geocoder lookup"

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -10,30 +10,48 @@ import MapKit
 
 struct ContentView: View {
     @State private var region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(
-                latitude: 42.15704,
-                longitude: -88.14812),
-            span: MKCoordinateSpan(
-                latitudeDelta: 0.05,
-                longitudeDelta: 0.05)
-        )
+        center: CLLocationCoordinate2D(
+            latitude: 42.15704,
+            longitude: -88.14812),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
     @StateObject var locationManager = LocationManager()
-        @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var places = [Place(name: "Barrington High School",
-                            coordinate: CLLocationCoordinate2D(
-                                latitude: 42.15704, longitude: -88.14812))]
+                                       coordinate: CLLocationCoordinate2D(
+                                        latitude: 42.15704, longitude: -88.14812))]
     var body: some View {
         Map(coordinateRegion: $region,
-                    interactionModes: .all,
-                    showsUserLocation: true,
-                    userTrackingMode: $userTrackingMode,
-                    annotationItems: places) { place in
-                    MapAnnotation(coordinate: place.coordinate,
-                                  anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
-                        Marker(name: place.name)
-                    }
-                }
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.coordinate,
+                          anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
+                Marker(name: place.name)
+            }
         }
+            .onAppear {
+                findLocation(name: "Springfield")
+            }
+    }
+    
+    func findLocation(name: String) {
+        locationManager.geocoder.geocodeAddressString(name) { (placemarks, error) in
+            guard placemarks != nil else {
+                print("Could not locate \(name)")
+                return
+            }
+            for placemark in placemarks! {
+                let place = Place(name: "\(placemark.name!), \(placemark.administrativeArea!)",
+                                  coordinate: placemark.location!.coordinate)
+                places.append(place)
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
